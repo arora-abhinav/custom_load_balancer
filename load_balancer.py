de77f786828fd8151d033d3d1cd7a52259675abe
Abhinav Arora <aroraabhinav2007@gmail.com>
2026-09-12 01:48:22 -0400

    Implemented concurrent requests being made to the health endpoint. Will implement the load balancer and actually sending out the requests every 3 seconds

--- load_balancer.py
+++ load_balancer.py
@@ -1,13 +1,41 @@
 from fastapi import FastAPI, HTTPException
 import requests
+import httpx
+import asyncio
 
-url_8001 = "http://0.0.0.0:8001/get_user_tier/u_1010"
-url_8002 = "http://0.0.0.0:8002/get_user_tier/u_1010"
-url_8000 = "http://0.0.0.0:8000/get_user_tier/u_1010"
+url_8001 = "http://0.0.0.0:8001/"
+url_8002 = "http://0.0.0.0:8002/"
+url_8000 = "http://0.0.0.0:8000/"
 
 urls = [url_8002, url_8001, url_8000]
 
-try:
+async def health_request(url):
+    #This is wrapped in a try except block to prevent the error from bubbling up and stopping 
+    #all other tasks in the main function
+    try:
+        #This specific context manager is used to dish out requests asynchronously. 
+        async with httpx.AsyncClient() as client:
+            #await is required here because client.get is running as an async def under the hood. Calling it without await 
+            #simply means the response is actually couroutine object that is simply being waited to be run
+            response = await client.get(url)
+    except httpx.ConnectError as e:
+        response = {"Health": False}
+    return response
+        
+async def main():
+    tasks = []
+    #Task Group merges tasks together to run asynchronously. Exits when all tasks are done
+    async with asyncio.TaskGroup() as tg:
+        for url in urls:
+            task = tg.create_task(health_request(url))
+            tasks.append(task)
+
+    print("Hit all endpoints")
+
+#Bridges the gap between asynchronous running and synchronous execution
+asyncio.run(main())
+
+"""try:
     for url in urls:
         response = requests.get(url)
         if response.status_code != 200:
@@ -16,5 +44,4 @@ try:
             print(response.json())
 except Exception:
     print("Failed")
-
-
+"""
